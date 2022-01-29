a574b646a2e5a2561d81c10b75781f134fe09ba5
ahngbeom <bbu0704@gmail.com>
2022-01-29 23:28:21 +0900

    Ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/29 22:05:44 by bahn              #+#    #+#             */
+/*   Updated: 2022/01/29 22:32:15 by bahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+static int	getNbAccounts( void ) {
+	
+}
+static int	getTotalAmount( void );
+static int	getNbDeposits( void );
+static int	getNbWithdrawals( void );
+static void	displayAccountsInfos( void );
