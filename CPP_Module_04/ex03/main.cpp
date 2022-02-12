1018257f72968b017b1274f2cee873c7685b1cdf
ahngbeom <bbu0704@gmail.com>
2022-02-13 03:04:03 +0900

    CPP Module 04 - ex03 ::: Abstract & Interface

--- CPP_Module_04/ex03/main.cpp
+++ CPP_Module_04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 15:12:46 by bahn              #+#    #+#             */
-/*   Updated: 2022/02/13 03:00:35 by bahn             ###   ########.fr       */
+/*   Updated: 2022/02/13 03:03:53 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,28 @@
 
 int main()
 {
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-	
-	ICharacter* me = new Character("me");
-	
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	
-	ICharacter* bob = new Character("bob");
-	
-	me->use(0, *bob);
-	me->use(1, *bob);
-	
-	delete bob;
-	delete me;
-	delete src;
-	
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		
+		ICharacter* me = new Character("me");
+		
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		
+		ICharacter* bob = new Character("bob");
+		
+		me->use(0, *bob);
+		me->use(1, *bob);
+		
+		delete bob;
+		delete me;
+		delete src;
+	}
+
 	return 0;
 }
