4570556a49de578d08d3f87334dc4798df5aa165
ahngbeom <bbu0704@gmail.com>
2022-02-18 15:39:37 +0900

    CPP Module 05 - ex02

--- CPP_Module_05/ex02/main.cpp
+++ CPP_Module_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 22:28:17 by bahn              #+#    #+#             */
-/*   Updated: 2022/02/18 15:03:44 by bahn             ###   ########.fr       */
+/*   Updated: 2022/02/18 15:39:16 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,54 +17,54 @@
 
 int main()
 {
-	Form*	home = new ShrubberyCreationForm("Home");
-	Form*	car = new RobotomyRequestForm("Car");
+	Form*	Home = new ShrubberyCreationForm("Home");
+	Form*	Car = new RobotomyRequestForm("Car");
 	Form*	Bane = new PresidentialPardonForm("Bane");
 	
-	Bureaucrat bahn("bahn", 140);
+	Bureaucrat Me("bahn", 140);
 	
 	std::cout << "======================================================================================" << std::endl;
 	
 	{
-		std::cout << "TEST #1 (Executer Grade : " << bahn.getGrade() << ")" << std::endl;
-		std::cout << "1 : \n"; bahn.executeForm(*home);
-		std::cout << "2 : \n"; bahn.executeForm(*car);
-		std::cout << "3 : \n"; bahn.executeForm(*Bane);
+		std::cout << "TEST #1 (Executer Grade : " << Me.getGrade() << ")" << std::endl;
+		std::cout << "1 : \n"; Me.executeForm(*Home);
+		std::cout << "2 : \n"; Me.executeForm(*Car);
+		std::cout << "3 : \n"; Me.executeForm(*Bane);
 		std::cout << "======================================================================================" << std::endl;
 	}
 
-	bahn.incrementTheGrade(50);
+	Me.incrementTheGrade(50);
 
 	{
-		std::cout << "TEST #2 (Executer Grade : " << bahn.getGrade() << ")" << std::endl;
-		std::cout << "1 : \n"; bahn.executeForm(*home);
-		std::cout << "2 : \n"; bahn.executeForm(*car);
-		std::cout << "3 : \n"; bahn.executeForm(*Bane);
+		std::cout << "TEST #2 (Executer Grade : " << Me.getGrade() << ")" << std::endl;
+		std::cout << "1 : \n"; Me.executeForm(*Home);
+		std::cout << "2 : \n"; Me.executeForm(*Car);
+		std::cout << "3 : \n"; Me.executeForm(*Bane);
 		std::cout << "======================================================================================" << std::endl;
 	}
 
-	bahn.incrementTheGrade(50);
+	Me.incrementTheGrade(50);
 
 	{
-		std::cout << "TEST #3 (Executer Grade : " << bahn.getGrade() << ")" << std::endl;
-		std::cout << "1 : \n"; bahn.executeForm(*home);
-		std::cout << "2 : \n"; bahn.executeForm(*car);
-		std::cout << "3 : \n"; bahn.executeForm(*Bane);
+		std::cout << "TEST #3 (Executer Grade : " << Me.getGrade() << ")" << std::endl;
+		std::cout << "1 : \n"; Me.executeForm(*Home);
+		std::cout << "2 : \n"; Me.executeForm(*Car);
+		std::cout << "3 : \n"; Me.executeForm(*Bane);
 		std::cout << "======================================================================================" << std::endl;
 	}
 
-	bahn.incrementTheGrade(35);
+	Me.incrementTheGrade(35);
 
 	{
-		std::cout << "TEST #4 (Executer Grade : " << bahn.getGrade() << ")" << std::endl;
-		std::cout << "1 : \n"; bahn.executeForm(*home);
-		std::cout << "2 : \n"; bahn.executeForm(*car);
-		std::cout << "3 : \n"; bahn.executeForm(*Bane);
+		std::cout << "TEST #4 (Executer Grade : " << Me.getGrade() << ")" << std::endl;
+		std::cout << "1 : \n"; Me.executeForm(*Home);
+		std::cout << "2 : \n"; Me.executeForm(*Car);
+		std::cout << "3 : \n"; Me.executeForm(*Bane);
 		std::cout << "======================================================================================" << std::endl;
 	}
 	
-	delete home;
-	delete car;
+	delete Home;
+	delete Car;
 	delete Bane;
 
 	return 0;
